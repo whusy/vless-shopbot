5f39849dd93edc4b49db782dc2890516f3dd2c04
Nikita <main@evansvl.ru>
2025-07-22 14:26:28 +0300

    updated payment handler

--- src/shop_bot/bot/handlers.py
+++ src/shop_bot/bot/handlers.py
@@ -371,10 +371,17 @@ async def create_yookassa_payment_handler(callback: types.CallbackQuery):
     message_id_to_delete = callback.message.message_id
     
     try:
+        if months == 1:
+            description = f"Оплата подписки на 1 месяц"
+        elif months <= 5:
+            description = f"Оплата подписки на {months} месяца"
+        else:
+            description = f"Оплата подписки на {months} месяцев"
+
         payment = Payment.create({
             "amount": {"value": price_rub, "currency": "RUB"},
             "confirmation": {"type": "redirect", "return_url": f"https://t.me/{TELEGRAM_BOT_USERNAME}"},
-            "capture": True, "description": f"Оплата подписки ({months} месяцев)",
+            "capture": True, "description": description,
             "metadata": {
                 "user_id": user_id, "months": months, "price": price_rub, 
                 "action": action, "key_id": key_id,
