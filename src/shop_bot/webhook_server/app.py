beb7cb6a1002e7100b4237869e0dcd984e71c261
Nikita <main@evansvl.ru>
2025-08-04 02:48:19 +0300

    hotfix

--- src/shop_bot/webhook_server/app.py
+++ src/shop_bot/webhook_server/app.py
@@ -25,7 +25,7 @@ ALL_SETTINGS_KEYS = [
     "panel_login", "panel_password", "about_text", "terms_url", "privacy_url",
     "support_user", "support_text", "channel_url", "telegram_bot_token",
     "telegram_bot_username", "admin_telegram_id", "yookassa_shop_id",
-    "yookassa_secret_key", "sbp_enabled"
+    "yookassa_secret_key", "sbp_enabled", "receipt_email"
 ]
 
 def create_webhook_app(bot_controller_instance):
